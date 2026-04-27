957cc140d21df1d32e3d9a9c0697743057dabf58
Valzorwyn <b.jankovics1015@gmail.com>
2026-04-27 21:43:18 +0200

    functions done, testing

---
+++ Libft/ft_lstmap.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new_lst;
+	t_list	*node;
+
+	new_lst = NULL;
+	while (lst)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&new_lst, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new_lst, node);
+		lst = lst->next;
+	}
+	return (new_lst);
+}
