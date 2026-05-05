37244ea6f14793cefd851df6e3c3e16f43fdd8ca
Balint Jankovics <bajankov@c3r11p3.42vienna.com>
2026-05-05 15:21:24 +0200

    bugfix

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -28,18 +28,16 @@ static int	ft_inset(char c, char const *set)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	int		ii;
-	int		jj;
 	char	*new;
 	int		len;
 
 	len = ft_strlen(s1) - 1;
 	ii = 0;
-	jj = 0;
 	while (s1[ii] && ft_inset(s1[ii], set))
 		ii++;
 	while (s1[len] && ft_inset(s1[len], set))
 		len--;
 	len++;
-	new = ft_substr(s1, ii, len - ii - jj);
+	new = ft_substr(s1, ii, len - ii);
 	return (new);
 }
