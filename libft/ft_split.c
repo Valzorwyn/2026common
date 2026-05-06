a8a38cb1d2352fc4ea2dca432dae5baaa201257b
Valzorwyn <b.jankovics1015@gmail.com>
2026-05-06 11:11:36 +0200

    split fix

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -14,32 +14,28 @@
 
 static int	ft_split_is_sep(char c, char delimiter)
 {
-	if (c == delimiter)
-		return (1);
-	return (0);
+	return (c == delimiter);
 }
 
 static int	ft_split_sep_count(char const *str, char c)
 {
-	int	ii;
-	int	sep_count;
+	int	count;
+	int	new_word;
 
-	sep_count = 0;
-	ii = 0;
-	while (str[ii])
+	count = 0;
+	new_word = 0;
+	while (*str)
 	{
-		if (!ft_split_is_sep(str[ii], c))
+		if (!ft_split_is_sep(*str, c) && !new_word)
 		{
-			sep_count++;
-			while (str[ii] && !ft_split_is_sep(str[ii], c))
-				ii++;
+			new_word = 1;
+			count++;
 		}
-		else
-		{
-			ii++;
-		}
+		else if (ft_split_is_sep(*str, c))
+			new_word = 0;
+		str++;
 	}
-	return (sep_count);
+	return (count);
 }
 
 static char	*ft_split_strdup(char const *src, char c)
@@ -51,37 +47,50 @@ static char	*ft_split_strdup(char const *src, char c)
 	src_len = 0;
 	while (src[src_len] && !ft_split_is_sep(src[src_len], c))
 		src_len++;
-	word = malloc((src_len + 1) * sizeof (char));
+	word = malloc((src_len + 1) * sizeof(char));
 	if (!word)
 		return (NULL);
-	ii = -1;
-	while (++ii < src_len)
+	ii = 0;
+	while (ii < src_len)
+	{
 		word[ii] = src[ii];
-	return (word[src_len] = '\0', word);
+		ii++;
+	}
+	word[src_len] = '\0';
+	return (word);
+}
+
+static char	**ft_split_free(char **res, int n)
+{
+	while (n--)
+		free(res[n]);
+	free(res);
+	return (NULL);
 }
 
 char	**ft_split(char const *str, char c)
 {
+	char	**res;
 	int		ii;
 	int		jj;
-	int		sep_count;
-	char	**res;
 
-	sep_count = ft_split_sep_count(str, c);
-	ii = 0;
-	jj = 0;
-	res = malloc((sep_count + 1) * sizeof(char *));
+	res = malloc((ft_split_sep_count(str, c) + 1) * sizeof(char *));
 	if (!res)
 		return (NULL);
+	ii = 0;
+	jj = 0;
 	while (str[ii])
 	{
 		while (str[ii] && ft_split_is_sep(str[ii], c))
-			++ii;
-		while (str[ii] && !ft_split_is_sep(str[ii], c))
+			ii++;
+		if (str[ii] && !ft_split_is_sep(str[ii], c))
 		{
-			res[jj++] = ft_split_strdup(&str[ii], c);
+			res[jj] = ft_split_strdup(&str[ii], c);
+			if (!res[jj])
+				return (ft_split_free(res, jj));
+			jj++;
 			while (str[ii] && !ft_split_is_sep(str[ii], c))
-				++ii;
+				ii++;
 		}
 	}
 	return (res[jj] = NULL, res);
